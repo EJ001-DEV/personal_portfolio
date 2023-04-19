e6fabe12865d506a5dd1e3258547a7958d1893d2
Edward <ing_jorge_caceres@hotmail.com>
2023-04-19 10:42:28 -0400

    changing setting.py .media/

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -155,7 +155,7 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '.media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
