aaebad6175171b5bcfe2bd758706b6b65d37edee
Edward <ing_jorge_caceres@hotmail.com>
2023-04-19 12:01:14 -0400

    change settings.py MEDIA_ROOT and TEMPLATE_DIRS

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -153,9 +153,13 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_ROOT = BASE_DIR / 'media'
+#MEDIA_ROOT = BASE_DIR / 'media'
 
 MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates/'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
