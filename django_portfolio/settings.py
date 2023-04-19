bf55f22a0e66c32671d80d1fd590b35388b5ffd7
Edward <ing_jorge_caceres@hotmail.com>
2023-04-19 16:30:26 -0400

    change setting.py for test1

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Following settings only make sense on production and may break development environments.
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
