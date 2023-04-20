5bee43b64200f687ef3539b6140c7253ebfad5a7
Edward <ing_jorge_caceres@hotmail.com>
2023-04-20 19:14:59 -0400

    modify settings.py try to repair statics configuration

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -168,10 +168,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 #STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 
 
@@ -185,9 +185,11 @@ STATIC_URL = '/static/'
 
 #MEDIA_ROOT = BASE_DIR / 'media'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 MEDIA_URL = 'media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 #TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates/'),)
 
