45fa528f94cc65e54c003bf7c7136ccae0a5485c
Edward <ing_jorge_caceres@hotmail.com>
2023-04-21 19:45:09 -0400

    modify settings.py from django docs

--- build.sh
+++ build.sh
@@ -5,12 +5,7 @@ echo "Building the project"
 pip install virtualenv
 virtualenv venv
 source venv/bin/activate
-python3.9 -m pip install --upgrade pip
-
 python3.9 -m pip install -r requirements.txt
-
-
-#ENV PIP_ROOT_USER_ACTION=ignore
 
 echo "Make Migration..."
 
@@ -19,13 +14,3 @@ python3.9 manage.py migrate --noinput
 
 echo "Collect Static"
 python3.9 manage.py collectstatic --noinput --clear
-
-#python manage.py findstatic code.png
-
-#chmod 777 /vercel/path0/staticfiles_build/static/
-
-#echo "Give permissions"
-#chmod 777 "/vercel/path0/staticfiles_build/static/"
-#chmod 777 "/vercel/path0/"
-
-#chmod 777 "/var/"
