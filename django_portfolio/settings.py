0fcccea1dd77c9918a4ca05cf9ca83c4cb888203
Edward <ing_jorge_caceres@hotmail.com>
2023-04-20 21:33:51 -0400

    modify vercel.json and settings.py2

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -90,7 +90,7 @@ ROOT_URLCONF = 'django_portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
