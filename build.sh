4e1413ec34221f061f8805a94caf8a0449911610
Edward <ing_jorge_caceres@hotmail.com>
2023-04-21 06:25:23 -0400

    modify vercel.json to add media folder

--- build.sh
+++ build.sh
@@ -11,4 +11,4 @@ python3.9 manage.py migrate --noinput
 echo "Collect Static"
 python3.9 manage.py collectstatic --noinput --clear
 
-chmod 777 /vercel/path0/staticfiles_build/static/
+#chmod 777 /vercel/path0/staticfiles_build/static/
