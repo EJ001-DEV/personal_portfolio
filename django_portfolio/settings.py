902c5675b29d4b3f7278293d4dd30738802639a3
Edward <ing_jorge_caceres@hotmail.com>
2023-04-21 15:38:37 -0400

    modify settings.py from django docs

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -174,7 +174,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static/',]
+#STATICFILES_DIRS = [BASE_DIR / 'static/',]
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 #STATIC_URL = 'static/'
