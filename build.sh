18eff6dcbe4d3641bf618f652b0ead445b42bde2
Edward <ing_jorge_caceres@hotmail.com>
2023-04-20 21:03:34 -0400

    modify vercel.json and settings.py

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
 echo "Collect Static"
-python3.9 manage.py collecstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
